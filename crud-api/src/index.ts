bc693fce12bf786ca9edab9473ee3fde09e2e206
Valentin Ak <vaakimov@gmail.com>
2023-01-15 20:11:03 +0300

    feat: add build

---
+++ crud-api/src/index.ts
@@ -0,0 +1,7 @@
+// import { chdir, argv } from "process";
+// import { homedir } from "os";
+// import { handleInput } from "./handlers/inputHandler.js";
+
+console.log('Welcome to the Crud Api');
+
+// handleInput(name);
